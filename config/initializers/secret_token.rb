c2dc63b1d6a93e195f3a8691112c41f574b9429f
jleo3 <joseph.leo3@gmail.com>
2013-10-17 08:50:25 -0400

    First Commit Configured with rspec-rails, capybara, simplecov, and shoulda-matchers

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GamesOnRails::Application.config.secret_key_base = '0d169a9073318ef4f65b77f7717402eae0f0c7bd4094122b181cce58bd6ca65bcf4bb2500b0068db63deb3c4030937caba975c9f78489c542f74f3017c43f422'
