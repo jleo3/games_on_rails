20a6ee2931c9ed22d765ec5409ebbcc7c160e589
jleo3 <joseph.leo3@gmail.com>
2013-10-18 13:29:43 -0400

    implementing view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,19 @@ module ApplicationHelper
   def secret_number_winner?(winning_choice, user_choice)
     winning_choice == user_choice
   end
+
+  def rock_paper_scissors_result(user_throw, computer_throw)
+    return :tie if user_throw == computer_throw
+    beats = {
+        "rock" => "scissors",
+        "scissors" => "paper",
+        "paper" => "rock"
+      }
+
+    if beats[user_throw] == computer_throw
+      :winner
+    else
+      :loser
+    end
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -10,4 +10,40 @@ describe ApplicationHelper do
       secret_number_winner?(1, 2).should be_false
     end
   end
+
+  describe "rock_paper_scissors_result" do
+    describe ":winner" do
+      it "rock beats scissors" do
+        rock_paper_scissors_result("rock", "scissors").should eq(:winner)
+      end
+
+      it "scissors beats paper" do
+        rock_paper_scissors_result("scissors", "paper").should eq(:winner)
+      end
+
+      it "paper beats rock" do
+        rock_paper_scissors_result("paper", "rock").should eq(:winner)
+      end
+    end
+
+    describe ":loser" do
+      it "rock loses to paper" do
+        rock_paper_scissors_result("rock", "paper").should eq(:loser)
+      end
+
+      it "scissors loses to rock" do
+        rock_paper_scissors_result("scissors", "rock").should eq(:loser)
+      end
+
+      it "paper loses to scissors" do
+        rock_paper_scissors_result("paper", "scissors").should eq(:loser)
+      end
+    end
+
+    describe ":tie" do
+      it "rock ties rock" do
+        rock_paper_scissors_result("rock", "rock").should eq(:tie)
+      end
+    end
+  end
 end
