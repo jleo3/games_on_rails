14e155475401d378fbbc63dbef19249cb3e540c1
jleo3 <joseph.leo3@gmail.com>
2013-10-18 13:06:15 -0400

    controller specs, instance variables set

--- app/controllers/rock_paper_scissors_controller.rb
+++ app/controllers/rock_paper_scissors_controller.rb
@@ -1,5 +1,17 @@
 class RockPaperScissorsController < ApplicationController
   def new
-    @throws = %w(rock paper scissors)
+    @throws = get_throws
+  end
+
+  def show
+    @throws = get_throws
+    @user_throw = @throws[params[:id].to_i]
+    @game_throw = @throws[rand(3)]
+  end
+
+  private
+
+  def get_throws
+    %w(rock paper scissors)
   end
 end
--- spec/controllers/rock_paper_scissors_controller_spec.rb
+++ spec/controllers/rock_paper_scissors_controller_spec.rb
@@ -1,14 +1,27 @@
 require 'spec_helper'
 
 describe RockPaperScissorsController do
+  before do
+    @choices = %w(paper rock scissors)
+  end
+
   describe "GET 'new'" do
     it "should be successul" do
       get 'new'
 
       response.should be_success
-      assigns(:throws).should include("rock")
-      assigns(:throws).should include("paper")
-      assigns(:throws).should include("scissors")
+      assigns(:throws).should =~ @choices
+    end
+  end
+
+  describe "GET 'show'" do
+    it "should be successul" do
+      get 'show', id: "rock"
+
+      response.should be_success
+      assigns(:throws).should =~ @choices
+      assigns(:user_throw).should eq("rock")
+      @choices.should include(assigns(:game_throw))
     end
   end
 end
